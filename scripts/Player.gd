86ab00584ac6b98b65e35517e9b98ef2bc0bd6e0
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-08-30 14:45:54 +0200

    Implemented basic player

---
+++ scripts/Player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the x-axis input direction and handle the movement/deceleration.
+	var x_movement := Input.get_axis("ui_left", "ui_right")
+	if x_movement:
+		velocity.x = x_movement * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Get the y-axis input direction and handle the movement/deceleration.
+	var y_movement := Input.get_axis("ui_up", "ui_down")
+	if y_movement:
+		velocity.y = y_movement * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	move_and_slide()
