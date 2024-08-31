03dbc2cde07a737beeb2a3dc2101a99063225894
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-01 01:15:31 +0200

    Fade out music on scene change

--- scripts/MenuMusic.gd
+++ scripts/MenuMusic.gd
@@ -2,6 +2,8 @@ extends AudioStreamPlayer
 
 var target_volume : float
 
+var scene_transition = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	target_volume = volume_db
@@ -11,7 +13,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Fade in volume
-	if volume_db < target_volume:
+	if volume_db < target_volume and not scene_transition:
 		volume_db += delta * 10
 		if volume_db > target_volume:
 			volume_db = target_volume
+			
+	elif scene_transition:
+		volume_db -= delta * 10
+
+func _on_start_button_clicked():
+	scene_transition = true
