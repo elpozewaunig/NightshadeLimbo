0b1dcc4fcbf93b09a33a0e6644635d67498854d8
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-01 21:46:20 +0200

    Hide occluder when obstacle is destroyed

--- scripts/Obstacle.gd
+++ scripts/Obstacle.gd
@@ -1,11 +1,13 @@
 extends StaticBody2D
 
-@onready var sprite = $Sprite2D
+@onready var sprite = $AnimatedSprite2D
 @onready var collider = $CollisionShape2D
+@onready var occluder = $LightOccluder2D
 
 func _ready() -> void:
 	sprite.frame = 0
 
 func destroy() -> void:
 	sprite.frame = 1
+	occluder.hide()
 	collider.set_deferred("disabled", true)
