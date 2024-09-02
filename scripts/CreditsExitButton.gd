1ec1665aa3bc05d4b87c236907ade4487bb87f92
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-02 21:29:50 +0200

    Fixed credits exit button being active while hidden

--- scripts/CreditsExitButton.gd
+++ scripts/CreditsExitButton.gd
@@ -4,6 +4,6 @@ extends Area2DButton
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super._process(delta)
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("ui_cancel") and is_visible_in_tree():
 		emit_signal("clicked")
 		click_sfx.play()
