e87d3daee91128a2e4bb99637badfa741935acbd
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-02 21:37:46 +0200

    Play crumble sound effect when obstacle is destroyed

--- scripts/Obstacle.gd
+++ scripts/Obstacle.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 @onready var sprite = $AnimatedSprite2D
 @onready var collider = $CollisionShape2D
 @onready var occluder = $LightOccluder2D
+@onready var break_sfx = $BreakSFX
 
 func _ready() -> void:
 	sprite.frame = 0
@@ -10,4 +11,5 @@ func _ready() -> void:
 func destroy() -> void:
 	sprite.frame = 1
 	occluder.hide()
+	break_sfx.play()
 	collider.set_deferred("disabled", true)
