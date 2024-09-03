3f5474279b0009f517ae7fabf7e7d3402a8613c2
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-03 03:47:25 +0200

    Changed loading animation

--- scripts/LoadingDots.gd
+++ scripts/LoadingDots.gd
@@ -4,20 +4,20 @@ extends Node2D
 
 var dots : Array[Sprite2D] = []
 var modulate_step = 0
-var disappear = false
+var disappear = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for child in get_children():
 		dots.append(child)
-		child.modulate.a = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	modulate_step += delta * speed
+	
+	# Make dots fade in one by one
 	if not disappear:
-		modulate_step += delta * speed
-		
 		for i in range(0, dots.size()):
 			if modulate_step >= i:
 				dots[i].modulate.a = modulate_step - i
@@ -27,11 +27,15 @@ func _process(delta: float) -> void:
 		if modulate_step >= dots.size():
 			disappear = true
 			modulate_step = 0
-			
 	
+	# Make dots fade out one by one
 	if disappear:
-		for dot in dots:
-			dot.modulate.a -= delta * speed
-			if dot.modulate.a <= 0:
-				dot.modulate.a = 0
-				disappear = false
+		for i in range(0, dots.size()):
+			if modulate_step >= i:
+				dots[i].modulate.a = 1 - (modulate_step - i)
+				if dots[i].modulate.a <= 0:
+					dots[i].modulate.a = 0
+		
+		if modulate_step >= dots.size():
+			disappear = false
+			modulate_step = 0
