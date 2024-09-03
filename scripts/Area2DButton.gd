ca7d9fb8a8de3d3758877d5533ad957c021839fc
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-03 22:01:23 +0200

    Made credits exit button selectable with key input

--- scripts/Area2DButton.gd
+++ scripts/Area2DButton.gd
@@ -1,15 +1,17 @@
 extends Area2D
 class_name Area2DButton
 
-var mouse_inside = false
-var disabled = false
-var highlight_active = false
-var ext_selected = false
-
 @onready var sprite = $AnimatedSprite2D
 @onready var label = $Label
 @onready var click_sfx = $ClickSFX
 @onready var select_sfx = $SelectSFX
+
+var mouse_inside = false
+var disabled = false
+var highlight_active = false
+var ext_selected = false
+var prev_visible = false
+var require_release = true
 
 signal clicked
 signal selected(button)
@@ -20,17 +22,29 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	
+	# If the button was just shown, require input to be released before it can be clicked
+	if is_visible_in_tree() != prev_visible:
+		require_release = true
+		prev_visible = is_visible_in_tree()
+	
+	# If the button is selected (either through the mouse or key input)
 	if is_visible_in_tree() and ((mouse_inside and not highlight_active) or (ext_selected and highlight_active)) and not disabled:
 		modulate = Color("ff7070")
 		sprite.frame = 1
 		
-		if Input.is_action_just_pressed("ui_click") or Input.is_action_just_pressed("ui_select"):
+		if Input.is_action_just_pressed("ui_click") or Input.is_action_just_pressed("ui_select") and not require_release:
 			emit_signal("clicked")
 			click_sfx.play()
 			
+	# Button is not selected
 	else:
 		modulate = Color("ffffff")
 		sprite.frame = 0
+	
+	# If no button click input is pressed, a release has been performed
+	if not Input.is_action_pressed("ui_click") and not Input.is_action_pressed("ui_select"):
+		require_release = false
 
 
 func _on_mouse_entered() -> void:
