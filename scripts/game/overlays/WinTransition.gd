865c813993c778ceb4e9a3a59082a203cc101ee9
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-10 04:34:09 +0200

    Made transition to ending cutscene smoother

--- scripts/game/overlays/WinTransition.gd
+++ scripts/game/overlays/WinTransition.gd
@@ -1,8 +1,14 @@
 extends Sprite2D
 
 @export var duration : float = 3.0
+@export var ftb_duration : float = 1.0
 
-var transition = false
+var transition : bool = false
+var fade_to_black : bool = false
+
+var ftb_time_elapsed : float = 0
+
+signal disable_lights
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +18,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if transition:
-		modulate.a += delta / duration
-		if modulate.a >= 1:
-			modulate.a = 1
-			transition = false
-			SceneManager.change_scene(SceneManager.endscene_scene)
 		
+		# Fade white screen in
+		if not fade_to_black:
+			modulate.a += delta / duration
+			if modulate.a >= 1:
+				modulate.a = 1
+				fade_to_black = true
+				disable_lights.emit()
+		
+		# Gradually change white to black through modulate
+		if fade_to_black:
+			ftb_time_elapsed += delta
+			modulate = Color("ffffff").lerp(Color("000000"), ftb_time_elapsed / ftb_duration)
+			
+			if ftb_time_elapsed >= ftb_duration:
+				modulate = Color("000000")
+				transition = false
+				SceneManager.change_scene(SceneManager.endscene_scene)
 
 
 func _on_player_exited() -> void:
