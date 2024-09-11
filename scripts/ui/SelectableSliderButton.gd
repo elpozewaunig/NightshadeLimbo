7d5d8f8ec9df5ed772a056ca04a288cb3f3f29d7
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-11 19:41:56 +0200

    Added acoustic feedback when adjusting volumes

--- scripts/ui/SelectableSliderButton.gd
+++ scripts/ui/SelectableSliderButton.gd
@@ -3,6 +3,10 @@ extends SelectableButton
 signal value_changed(value)
 
 @onready var slider = $SliderButtonSrc
+@onready var slider_sfx = $SliderSFX
 
 func _on_slider_value_changed(value: float) -> void:
 	value_changed.emit(value)
+	
+	if not slider_sfx.playing:
+		slider_sfx.play()
