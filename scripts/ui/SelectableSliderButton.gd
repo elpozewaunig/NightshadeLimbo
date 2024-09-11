4102466ad3e7b4a5d7ee2fca6aba0ccf2ac618f7
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-09-11 19:48:34 +0200

    Added acoustic feedback toggle

--- scripts/ui/SelectableSliderButton.gd
+++ scripts/ui/SelectableSliderButton.gd
@@ -2,11 +2,13 @@ extends SelectableButton
 
 signal value_changed(value)
 
+@export var acoustic_feedback : bool = true
+
 @onready var slider = $SliderButtonSrc
 @onready var slider_sfx = $SliderSFX
 
 func _on_slider_value_changed(value: float) -> void:
 	value_changed.emit(value)
 	
-	if not slider_sfx.playing:
+	if acoustic_feedback and not slider_sfx.playing:
 		slider_sfx.play()
