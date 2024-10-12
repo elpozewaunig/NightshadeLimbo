855fac14e9c20500ec943a28239307a9c230cd4c
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-10-12 20:59:19 +0200

    Made HealthIndicator show preview in editor

--- scripts/game/overlays/HealthIndicator.gd
+++ scripts/game/overlays/HealthIndicator.gd
@@ -1,8 +1,5 @@
+@tool
 extends Node2D
-
-var hearts : Array[Sprite2D] = []
-
-var placement_pos_x = 0
 
 @export var src_node : Node2D
 @export var heart_texture : Texture2D
@@ -12,13 +9,29 @@ var placement_pos_x = 0
 
 @onready var box = $Box
 
-var max_health
+var max_health : int
+var prev_src_node : Node2D = src_node
+var prev_max_health : int
+
+var hearts : Array[Sprite2D] = []
+
+var placement_pos_x : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	max_health = src_node.health
+	if src_node != null:
+		max_health = src_node.health
+	else:
+		max_health = 0
+	
+	prev_src_node = src_node
+	prev_max_health = max_health
 	placement_pos_x = - max_health * spacing / 2
 	
+	for heart in hearts:
+		remove_child(heart)
+	hearts.clear()
+		
 	for i in range(0, max_health):
 		var heart = Sprite2D.new()
 		heart.scale.x = heart_scale
@@ -36,8 +49,14 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	for i in range(0, hearts.size()):
-		if i < src_node.health:
-			hearts[i].texture = heart_texture
-		else:
-			hearts[i].texture = no_heart_texture
+	if Engine.is_editor_hint():
+		# Update display when src node changes or health changes in editor
+		if src_node != prev_src_node or (src_node != null and src_node.health != prev_max_health):
+			_ready()
+	
+	if visible:
+		for i in range(0, hearts.size()):
+			if i < src_node.health:
+				hearts[i].texture = heart_texture
+			else:
+				hearts[i].texture = no_heart_texture
